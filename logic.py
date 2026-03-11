4d392b20b0d50c3be2f73e683a2d9d12b38e3ed6
Marvin2264 <Marvingorbach500@gmail.com>
2026-03-11 20:48:55 +0100

    second commit outputfunction

--- logic.py
+++ logic.py
@@ -1,43 +1,43 @@
-def output(boardlist):
+def output(bordlist):
+
     ENDC        = "\033[0m"        
     RED         = "\033[91m"
     GREEN       = "\033[92m"
     rowcounter  = 0
 
-    if wintest(boardlist, player1) == True:
-          print(boardlist)
+    for i in bordlist:
+        for y in i:
+            if y == "X":
+                if rowcounter < 2:
+                    print(f'{RED}{y}{ENDC}', end="")
+                    print(" | ", end="")
+                    rowcounter += 1
+                else:
+                    print(f'{RED}{y}{ENDC}')
+                    rowcounter = 0
+
+            elif y == "O":
+                if rowcounter < 2:
+                    print(f'{GREEN}{y}{ENDC}', end="")
+                    print(" | ", end="")
+                    rowcounter += 1
+                else:
+                    print(f'{GREEN}{y}{ENDC}')
+                    rowcounter = 0
+
+            else:
+                if rowcounter < 2:
+                    print(f'{y}', end="")
+                    print(" | ", end="")
+                    rowcounter += 1
+                else:
+                    print(f'{y}')
+                    rowcounter = 0
+
+    if wintest(bordlist, "player1") == True:
+          print(bordlist)
           print("Spieler 1 hat gewonnen!")
 
-    elif wintest(boardlist, player2) == True:
-          print(boardlist)
+    elif wintest(bordlist, "player2") == True:
+          print(bordlist)
           print("Spieler 2 hat gewonnen!")
-
-    else:
-          for i in boardlist:
-                for y in i:
-                    if y == "X":
-                        if rowcounter < 2:
-                            print(f'{RED}{y}{ENDC}', end="")
-                            print(" | ", end="")
-                            rowcounter = rowcounter + 1
-                        else:
-                            print(f'{RED}{y}{ENDC}')
-                            rowcounter = 0
-
-                    elif y == "O":
-                        if rowcounter < 2:
-                            print(f'{GREEN}{y}{ENDC}', end="")
-                            print(" | ", end="")
-                            rowcounter = rowcounter + 1
-                        else:
-                            print(f'{GREEN}{y}{ENDC}')
-                            rowcounter = 0
-
-                    else:
-                        if rowcounter < 2:
-                            print(f'{y}', end="")
-                            print(" | ", end="")
-                            rowcounter = rowcounter + 1
-                        else:
-                            print(f'{y}')
-                            rowcounter = 0
